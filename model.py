63d12742cb432bda0d22b23efdfbaece169ab4b6
Lok <huang.2870@osu.edu>
2019-01-06 04:01:07 -0500

    Change cnn model

--- model.py
+++ model.py
@@ -2,14 +2,14 @@ import tensorflow as tf
 import csv
 import os
     
-def model_fn(features, labels, mode):
+def cnn_model_fn(features, labels, mode):
     if type(features) is dict:
         features = features['features']
     input_layer = tf.reshape(features, [-1, 512, 512, 4])
 
     conv1 = tf.layers.conv2d(inputs = input_layer,
         filters = 32,
-        kernel_size = [5, 5],
+        kernel_size = [4, 4],
         padding = "same",
         activation = tf.nn.relu)
         
@@ -19,7 +19,7 @@ def model_fn(features, labels, mode):
         
     conv2 = tf.layers.conv2d(inputs = pool1,
         filters = 64,
-        kernel_size = [5, 5],
+        kernel_size = [4, 4],
         padding = "same",
         activation = tf.nn.relu)
 
@@ -39,30 +39,24 @@ def model_fn(features, labels, mode):
         
     # dropout
     
-    logits = tf.layers.dense(inputs = dense2, units = 2)
+    logits = tf.layers.dense(inputs = dense2, units = 28)
     
-    argmax = tf.argmax(input = logits, axis = 1)
+    sigmoid = tf.nn.sigmoid(logits)
     
-    softmax = tf.nn.softmax(logits, name = "softmax_tensor")
-        
     if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(mode = mode, predictions = argmax)
+        return tf.estimator.EstimatorSpec(mode = mode, predictions = sigmoid)
         
     labels = tf.cast(labels, tf.int32)
-    loss = tf.losses.sparse_softmax_cross_entropy(labels = labels, logits = logits)
+    loss = tf.losses.sigmoid_cross_entropy(multi_class_labels = labels, logits = logits)
 
     if mode == tf.estimator.ModeKeys.TRAIN:
         optimizer = tf.train.AdamOptimizer(learning_rate = 0.001)
         train_op = optimizer.minimize(loss = loss, global_step = tf.train.get_global_step())
         
-        accuracy = tf.metrics.accuracy(labels = labels, predictions = argmax)
-        log = {"accuracy" : accuracy[1]}
-        logging_hook = tf.train.LoggingTensorHook(tensors = log, every_n_iter = 100)
+        return tf.estimator.EstimatorSpec(mode = mode, loss = loss, train_op = train_op)
         
-        return tf.estimator.EstimatorSpec(mode = mode, loss = loss, train_op = train_op, training_hooks = [logging_hook])
-        
-    if mode == tf.estimator.ModeKeys.EVAL:
-        accuracy = tf.metrics.accuracy(labels = labels, predictions = argmax)
-        eval_metric_ops = {"accuracy" : accuracy}
-        tf.summary.scalar('accuracy', accuracy[1])
-        return tf.estimator.EstimatorSpec(mode = mode, loss = loss, eval_metric_ops = eval_metric_ops)
+    # if mode == tf.estimator.ModeKeys.EVAL:
+        # accuracy = tf.metrics.accuracy(labels = labels, predictions = argmax)
+        # eval_metric_ops = {"accuracy" : accuracy}
+        # tf.summary.scalar('accuracy', accuracy[1])
+        # return tf.estimator.EstimatorSpec(mode = mode, loss = loss, eval_metric_ops = eval_metric_ops)
